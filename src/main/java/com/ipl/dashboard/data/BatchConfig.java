aa91048d6742b5e906e0c24badd1c39f5a154537
Tanishka-dev <tanishakayadav2110@gmail.com>
2023-04-22 00:36:58 +0530

    Step  added

--- src/main/java/com/ipl/dashboard/data/BatchConfig.java
+++ src/main/java/com/ipl/dashboard/data/BatchConfig.java
@@ -1,8 +1,12 @@
 package com.ipl.dashboard.data;
 
 import com.ipl.dashboard.model.Match;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
@@ -14,6 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
 import javax.sql.DataSource;
 
 
@@ -58,6 +64,29 @@ public class BatchConfig {
                 .sql("INSERT INTO match (match_id,season,date,city,venue,team1,team2,toss_winner,toss_decision,player_of_match,match_winner,match_number,umpire1,umpire2,reserve_umpire,winner_wickets,winner_runs)"+
                         "VALUES(:matchId,:season,:date,:city,:venue,:team1,:team2,:tossWinner,:tossDecision,:playerOfMatch,:matchWinner,:matchNumber,:umpire1,:umpire2,:reserveUmpire,:winnerWickets,:winnerRuns)")
                 .dataSource(dataSource)
+                .build();
+    }
+
+    @Bean
+    public Job importUserJob(JobRepository jobRepository,
+                             JobCompletionNotificationListener listener, Step step1) {
+        return new JobBuilder("importUserJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .listener(listener)
+                .flow(step1)
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository,
+                      PlatformTransactionManager transactionManager, JdbcBatchItemWriter<Match> writer) {
+        return new StepBuilder("step1", jobRepository)
+                .<MatchInput, Match>
+                        chunk(10, transactionManager)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer)
                 .build();
     }
 }
